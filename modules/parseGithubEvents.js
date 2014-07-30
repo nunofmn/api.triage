0caec19e00f81a018797c6123f8380bd97ab9f92
David Dias <daviddias.p@gmail.com>
2014-07-29 17:09:39 -0700

    github-webook

--- modules/parseGithubEvents.js
+++ modules/parseGithubEvents.js
@@ -6,26 +6,26 @@ var handler = server.plugins['Github-Webhook'].events;
 
 handler.on('*', function (event){
   console.log('SOMETHING: \n', event);
-})
+});
 
 handler.on('ping', function (event){
   console.log('PING: \n', event);
-})
+});
 
 handler.on('error', function (err) {
-  console.err('Error:', err.message)
-})
+  console.err('Error:', err.message);
+});
 
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref)
-})
+    event.payload.ref);
+});
 
 handler.on('issues', function (event) {
   console.log('Received an issue event for % action=%s: #%d %s',
     event.payload.repository.name,
     event.payload.action,
     event.payload.issue.number,
-    event.payload.issue.title)
-}) 
+    event.payload.issue.title);
+});
