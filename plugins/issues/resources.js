909ffd7786e944205a3e6595207aff07037f36d5
Nuno Nogueira <nunofmn@gmail.com>
2014-09-14 17:41:54 +0100

    API now sends 400 Bad Request, when page number is superior to total pages.

--- plugins/issues/resources.js
+++ plugins/issues/resources.js
@@ -2,13 +2,13 @@ var Issue   = require('./../../store/models/issue.js');
 // var User    = store.models.Issue; CHANGE THIS
 var config  = require('config');
 var Hapi    = require('hapi');
+var Boom    = require('boom');
 var async   = require('async');
 
 exports.getIssues = function (request, reply) {
 
   var page = parseInt(request.query.page) || 1;
   var paginationfn = [];
-  console.log(request.info.host);
 
   // Function to get first page of issues
   var getFirstIssues = function(callback) {
@@ -24,7 +24,6 @@ exports.getIssues = function (request, reply) {
   // Function to get n page of issues
   var getIssuesPagination = function(pagetoken, models, callback) {
     Issue.all({sortBy: 'number', limit: 10, continuation: pagetoken.continuation}, function(err, models, pagination) {
-        console.log(pagination);
       if(err) {
         callback(err, null);
       }else {
@@ -39,12 +38,14 @@ exports.getIssues = function (request, reply) {
       var pageNumber = parseInt(req.query.page);
 
       // Next page link
-      if(pageNumber < totalPages)
+      if(pageNumber < totalPages) {
           linkField.push('<http://' + req.info.host + '/issues?page=' + (pageNumber+1) + '>; rel="next"');
+      }
 
       // Previous page link
-      if(pageNumber != 1)
+      if(pageNumber !== 1) {
           linkField.push('<http://' + req.info.host + '/issues?page=' + (pageNumber-1) + '>; rel="previous"');
+      }
 
       // First page link
       linkField.push('<http://' + req.info.host + '/issues?page=1; rel="first"');
@@ -52,23 +53,25 @@ exports.getIssues = function (request, reply) {
       // Last page link
       linkField.push('<http://' + req.info.host + '/issues?page=' + totalPages + '>; rel="last"');
 
-
       return linkField.join(', ');
   };
 
   // Necessary to always go through first page of issues
   paginationfn.push(getFirstIssues);
 
-  // Iterate through pages, until get required page 
+  // Iterate through pages, until get required page
   for(var i=0; i<(page-1); i++) {
     paginationfn.push(getIssuesPagination);
   }
 
   // Serial execution of functions
   async.waterfall(paginationfn, function(err, pagination, models) {
-    reply(models).header("Link", generateLinkHeader(request, pagination));
+      if(Math.ceil(parseInt(pagination.total)/10) <= page) {
+          reply(models).header('Link', generateLinkHeader(request, pagination));
+      }else {
+          reply(Boom.badRequest('Page dont exist.'));
+      }
   });
-
 
   // TODO paginate the issues as github does (in the header);
   // https://github.com/fritzy/dulcimer#all look at pagination feature
